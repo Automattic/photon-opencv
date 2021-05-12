8d3501f1afc9b7caa5fcc0e28d65376e6ba524a4
Flávio Zavan <flaviozavan@gmail.com>
2021-05-12 12:00:08 +0300

    Bump to 0.2.7

--- photon-opencv.cpp
+++ photon-opencv.cpp
@@ -964,7 +964,7 @@ int Photon_OpenCV::_gmagick_imgtype_truecolormatte = -1;
 
 extern "C" {
   PHPCPP_EXPORT void *get_module() {
-    static Php::Extension extension("photon-opencv", "0.2.6");
+    static Php::Extension extension("photon-opencv", "0.2.7");
 
     Php::Class<Photon_OpenCV> photon_opencv("Photon_OpenCV");
 
