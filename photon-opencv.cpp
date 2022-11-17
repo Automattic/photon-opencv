471e1409fcc70ab8c3a215d87d4dd91b47eaad0e
Flávio <flaviozavan@gmail.com>
2022-11-17 21:39:03 +0200

    Bump to 0.2.27

--- photon-opencv.cpp
+++ photon-opencv.cpp
@@ -1383,7 +1383,7 @@ cmsHPROFILE Photon_OpenCV::_srgb_profile = nullptr;
 
 extern "C" {
   PHPCPP_EXPORT void *get_module() {
-    static Php::Extension extension("photon-opencv", "0.2.26");
+    static Php::Extension extension("photon-opencv", "0.2.27");
 
     Php::Class<Photon_OpenCV> photon_opencv("Photon_OpenCV");
 
